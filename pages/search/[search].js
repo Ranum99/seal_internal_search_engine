2e9e45cbf19718d370d4762a9732d4c05f040baa
ranum99 <aleksanderranum99@hotmail.com>
2022-04-01 12:25:30 +0200

    import axios, start on api call to elasticsearch

--- pages/search/[search].js
+++ pages/search/[search].js
@@ -1,8 +1,36 @@
+import axios from "axios";
 import { useRouter } from "next/router";
+import { useEffect } from "react"
 
 const Search = () => {
   const router = useRouter();
   const { search } = router.query;
+
+  useEffect(() => {
+    getSearchResults()
+  }, [search])
+
+  const getSearchResults = async () => {
+    try {
+      const response = await axios({
+        method: 'GET',
+        url: `http://localhost:9200/pokemon/_search`,
+        data: {
+          query: {
+            match_phrase_prefix: {
+                Name: search
+            }
+          }
+        },
+        source_content_type: 'application/json'
+      })
+
+      console.log(response, "meem");
+    } catch (error) {
+      console.log(error)
+      console.warn("Can't retrieve data")
+    }
+  }
 
   return (
     <main>
